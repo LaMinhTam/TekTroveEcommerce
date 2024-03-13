b525f9870fb6cf1f8ddb4bbc5266b383426e95b5
laminhtam <laminhtam5523@gmail.com>
2024-03-13 23:15:37 +0700

    initialize customer screen

---
+++ TekTroveWebParent/TekTroveCustomer/src/main/java/com/tektrove/tektrovecustomer/security/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.tektrove.tektrovecustomer.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(login -> login.loginPage("/login").usernameParameter("email")
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .logout(logout -> logout.logoutSuccessUrl("/login?logout").permitAll());
+
+        return httpSecurity.build();
+    }
+}
